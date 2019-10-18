2e12a59aa70fa41d278f3b7e43d4621f1d21fdc8
Dmitriy Tyutyunik <dmitriy.tyutyunik@gmail.com>
2019-10-18 01:34:59 -0400

    Updates

---
+++ precache-manifest.f423a5860eb46062227ab615efe07fd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed7fae79aea5de87ba72c0671df17ed",
+    "url": "/ImageDetection/index.html"
+  },
+  {
+    "revision": "4d4cdfa814214b32c636",
+    "url": "/ImageDetection/static/css/main.69a6c00c.chunk.css"
+  },
+  {
+    "revision": "107e6c83a48311558265",
+    "url": "/ImageDetection/static/js/2.358d4a96.chunk.js"
+  },
+  {
+    "revision": "4d4cdfa814214b32c636",
+    "url": "/ImageDetection/static/js/main.30b14181.chunk.js"
+  },
+  {
+    "revision": "120a7a7205a90d4085be",
+    "url": "/ImageDetection/static/js/runtime-main.d03b4d54.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ImageDetection/static/media/logo.25bf045c.svg"
+  }
+]);
